01ec1e0f176b1e0edb0b8a0dc8f74a2daa244681
rizqiyaaatul <fithriyahrizqi@gmail.com>
2022-11-11 04:24:00 +0700

    update

--- game_objects.py
+++ game_objects.py
@@ -29,3 +29,27 @@ class Paddle:
     def reset(self):
         self.x = self.original_x
         self.y = self.original_y
+
+class Ball:
+    MAX_VEL = 5
+    COLOR = WHITE
+
+    def __init__(self, x, y, radius):
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+        self.y_vel = 0
+        self.x_vel *= -1
