9837cc8b7c715ffa2578769fb94f001b848d4ccd
rizqiyaaatul <fithriyahrizqi@gmail.com>
2022-11-11 04:31:26 +0700

    update lagi

--- game_objects.py
+++ game_objects.py
@@ -31,18 +31,18 @@ class Paddle:
         self.y = self.original_y
 
 class Ball:
-    MAX_VEL = 5
-    COLOR = WHITE
+    MAX_kecepatan = 5
+    warna = WHITE
 
     def __init__(self, x, y, radius):
         self.x = self.original_x = x
         self.y = self.original_y = y
         self.radius = radius
-        self.x_vel = self.MAX_VEL
+        self.x_vel = self.MAX_kecepatan
         self.y_vel = 0
 
     def draw(self, win):
-        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
+        pygame.draw.circle(win, self.warna, (self.x, self.y), self.radius)
 
     def move(self):
         self.x += self.x_vel
